e6999a2b6b363aab7810ac4b5e9b45ca76eca6b5
Fredrik Pekkarinen <fredrik.pekkarinen@gmail.com>
2020-03-03 15:23:39 +0100

    firebase boilerplate

---
+++ src/firebase/firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCEYWnhSzvYlXKBgilTSQB4O_cx_Tvt2jE",
+    authDomain: "front-end-ddc50.firebaseapp.com",
+    databaseURL: "https://front-end-ddc50.firebaseio.com",
+    projectId: "front-end-ddc50",
+    storageBucket: "front-end-ddc50.appspot.com",
+    messagingSenderId: "166260714765",
+    appId: "1:166260714765:web:c5431919472f7da5ca9444"
+
+}
+
+firebase.initializeApp(firebaseConfig)
+const auth = firebase.auth();
+const db = firebase.firestore();
+export { auth, db }
