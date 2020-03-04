474414c6706af6dbabd9dba0e776676ec7b7e3c4
Ina Nivald <inanivald@hotmail.com>
2020-03-04 13:02:53 +0100

    QG1-4 fixed link

--- src/Quiz.js
+++ src/Quiz.js
@@ -2,13 +2,16 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 
 function Quiz(props){
-    
+   const quizListItems = props.quizState.map(quiz => {
+        console.log(quiz)
+        return ( <li key={quiz.id}>
+            <Link to={'/quiz/' + quiz.id}>{quiz.quizTitle}</Link>
+        </li>)
+    })
     return (
-        <div>
-            <li>
-                <Link to={'/quiz/123'}>Quizz title</Link>
-            </li>
-        </div>
+        <ul>
+           {quizListItems}
+        </ul>
     )
 }
 
