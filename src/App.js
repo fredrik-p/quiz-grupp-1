28b35878721a29745197467cdec71f535be6fd40
Gunnar Abrahamsson <nvtesp@gmail.com>
2020-03-04 10:48:36 +0100

    Created getQuizzes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
 import React from 'react';
+import { db } from './firebase/firebase'
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends React.Component {
+
+	state = {
+		quizes: []
+	}
+	componentDidMount() {
+		this.getQuizzes()
+	}
+
+	getQuizzes = () => {
+		db.collection('quizes').get()
+			.then(querySnapshot => {
+				//clear Quizz data
+				this.setState({
+					quizes: []
+				})
+
+				//add new data to quizz
+				querySnapshot.forEach(doc => {
+					this.setState({
+						//Concat doc to array
+						quizes: [...this.state.quizes, doc.data()]
+					}) 
+				})
+			})
+	}
+
+	render() {
+		return (
+			<div className="App">
+				
+			</div>
+		);
+	}
 }
 
 export default App;
