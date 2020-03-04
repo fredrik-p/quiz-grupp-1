0a0e7f2ffb39de085ca751b3656cc7e7eae11ca4
Gunnar Abrahamsson <nvtesp@gmail.com>
2020-03-04 11:23:00 +0100

    GQ1-3 update quizzes in real time

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
+import React from 'react'
 import { Route, BrowserRouter, Link} from 'react-router-dom';
-
+import QuizList from './QuizList'
 function App() {
   return (
     <div className="App">
-
-      
+		<QuizList />
     </div>
   );
 }
