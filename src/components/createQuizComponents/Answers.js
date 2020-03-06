e49b2799d50fa2b0cdb437f6ce6b17e69271f68a
Ina Nivald <inanivald@hotmail.com>
2020-03-06 22:24:12 +0100

    working on create quiz page

---
+++ src/components/createQuizComponents/Answers.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMinusCircle } from '@fortawesome/free-solid-svg-icons'
+
+class Answers extends React.Component {
+    state = {
+        answerTitle: '',
+        isTrue: false,
+    }
+
+    handleInputChange = (e) => {
+		this.setState({
+            [e.target.id]: e.target.value,
+        })
+    }
+    
+    deleteAnswer = () => {
+        this.props.handleDeleteAnswer(this.props.answer)
+    }
+
+    handleAnswerChange = (e) => {
+        this.props.handleAnswerChange(e.target.value, this.props.answer)
+    }
+
+    render() {
+        return (
+            <div>
+            <label htmlFor="answerTitle">Answer</label>
+                <div className="input-group mt-1 mb-1">
+                <input type="text"
+                id="answerTitle"
+                aria-label="Answer"
+                placeholder="Type in answer"
+                className="form-control"
+                onChange={this.handleAnswerChange}
+                />
+                
+                {this.props.answers.length > 2 
+                    ?
+                        <div className="input-group-append">
+                            <span onClick={this.deleteAnswer} 
+                                className="input-group-text">
+                                <FontAwesomeIcon icon={faMinusCircle}
+                                />
+                            </span>
+                        </div> 
+                    : 
+                        ''
+                    }
+                </div>
+                <button type="submit" 
+                className="btn btn-success"
+                >
+                Correct?
+                </button>
+            </div>
+        )
+    }
+}
+
+export default Answers
